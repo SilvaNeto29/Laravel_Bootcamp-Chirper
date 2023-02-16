7664f6548d1aef153443aecf3cbbd1befb1d1004
j.soares <joao.soares@rbmweb.com.br>
2023-02-16 11:03:22 -0300

    fix: namespace fix

--- app/Models/Chirp.php
+++ app/Models/Chirp.php
@@ -4,8 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
 
-class Chirp extends Model
+class Chirp extends User
 {
     use HasFactory;
 }
