bddc85bd66589dbb634cb45269e876a5b6528f51
j.soares <joao.soares@rbmweb.com.br>
2023-02-16 16:00:02 -0300

    fix: correcoes model

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -16,7 +16,7 @@ class ChirpController extends Controller
      */
     public function index()
     {
-        return view('chirps.index');
+        return view('chirps.index', ['chirps' => Chirp::with('user')->latest()->get(),]);
     }
 
     /**
--- app/Models/Chirp.php
+++ app/Models/Chirp.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\User;
 
-class Chirp extends User
+class Chirp extends Model
 {
     use HasFactory;
 }
