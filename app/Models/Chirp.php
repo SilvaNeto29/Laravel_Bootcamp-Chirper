0f01735011ff2324af0754939d86271c28d8c58d
j.soares <joao.soares@rbmweb.com.br>
2023-02-17 13:53:19 -0300

    fix: correcao chirp controller

--- app/Models/Chirp.php
+++ app/Models/Chirp.php
@@ -5,8 +5,15 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\User;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
-class Chirp extends Model
+class Chirp extends User
 {
     use HasFactory;
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
 }
